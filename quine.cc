0fecb50ad55b4051381b42d4fb56e6a8f6489bf1
david <david@davids-MacBook-Pro.local>
2022-01-23 19:18:51 -0800

    fixups

--- quine.cc
+++ quine.cc
@@ -3,24 +3,21 @@ int main() {
     using namespace std;
     char q = 34;
     char c = 44;
-    string str[21] = {
+    string str[18] = {
 "#include <iostream>",
 "int main() {",
 "    using namespace std;",
 "    char q = 34;",
 "    char c = 44;",
-"    string str[21] = {",
+"    string str[18] = {",
 "    };",
 "    for (int i = 0; i < 6; i++) {",
 "        cout<<str[i]<<endl;",
 "    }",
-"    for (int i = 0; i < 6; i++) {",
+"    for (int i = 0; i < 18; i++) {",
 "        cout<<q+str[i]+q+c<<endl;",
 "    }",
-"    for (int i = 6; i < 21; i++) {",
-"        cout<<q+str[i]+q+c<<endl;",
-"    }",
-"    for (int i = 6; i < 21; i++) {",
+"    for (int i = 6; i < 18; i++) {",
 "        cout<<str[i]<<endl;",
 "    }",
 "    return 0;",
@@ -29,13 +26,10 @@ int main() {
     for (int i = 0; i < 6; i++) {
         cout<<str[i]<<endl;
     }
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 18; i++) {
         cout<<q+str[i]+q+c<<endl;
     }
-    for (int i = 6; i < 21; i++) {
-        cout<<q+str[i]+q+c<<endl;
-    }
-    for (int i = 6; i < 21; i++) {
+    for (int i = 6; i < 18; i++) {
         cout<<str[i]<<endl;
     }
     return 0;
